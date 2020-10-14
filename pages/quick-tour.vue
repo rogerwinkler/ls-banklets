518513aec6ab04328c47fd41de1e5051ea5211af
rw <roger.winkler@monex.li>
2020-10-14 10:20:58 +0200

    quick tour v1

--- pages/quick-tour.vue
+++ pages/quick-tour.vue
@@ -4,16 +4,83 @@
       <Nav :title="$t('quick-tour')" backbutton="true" />
     </div>
     <div class="content">
-      <Logo />
-      <h1 class="title">
-        Banklets
-      </h1>
+      <div class="logo">
+        <Logo />
+        <h1 class="title">
+          Banklets
+        </h1>
+      </div>
+      <hr />
+      <div class="div-qt">
+        <h1 class="qt">Quick Tour</h1>
+      </div>
+      <!-- <hr /> -->
+      <div class="qt-text">
+        {{ $t("swipe-through-the-tour-") }}
+      </div>
+      <div class="div-btn">
+        <button @click="start">Beginne die Tour...</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    start() {
+      // console.log("quick-tour.vue::start");
+      this.$router.push("slide-show");
+    }
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.logo {
+  margin-top: 30px;
+  margin-bottom: 25px;
+}
+
+.div-qt {
+  margin-top: 25px;
+}
+
+.qt {
+  color: var(--secondary-color);
+  font-size: 54px;
+  font-weight: 800;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.qt-text {
+  color: var(--txt-color);
+  font-size: 20px;
+  text-align: left;
+  width: 80%;
+  margin-left: 10%;
+}
+
+hr {
+  width: 80%;
+  margin-left: 10%;
+}
+
+.div-btn button {
+  color: var(--txt-color);
+  border: 0;
+  background-color: white;
+  font-size: 24px;
+  border: none;
+  border-radius: 4px;
+  background-color: white;
+  box-shadow: -6px 4px 10px var(--secondary-color);
+  width: 80%;
+  margin-top: 20px;
+}
+
+.div-btn button:hover {
+  background-color: var(--secondary-color);
+}
+</style>
---
+++ pages/slide-show.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="container">
+    <div class="nav">
+      <nav class="site-navigation">
+        <div class="div-btn-left">
+          <button class="btn-left" @click="goBack">
+            <img
+              id="back-button"
+              src="/img/313-arrow-left-grey.svg"
+              alt="Arrow left"
+            />
+          </button>
+        </div>
+        <div class="div-nav-title">
+          {{ $t("quick-tour") }}
+        </div>
+        <div class="div-btn-stop">
+          <button class="btn-stop" @click="toggleMenu">
+            <img src="/img/287-stop2-grey.svg" alt="Menu" />
+          </button>
+        </div>
+        <div class="div-btn-right">
+          <button class="btn-right" @click="toggleMenu">
+            <img src="/img/309-arrow-right-grey.svg" alt="Menu" />
+          </button>
+        </div>
+      </nav>
+    </div>
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+nav {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 40px;
+}
+
+.div-btn-left {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.div-btn-right {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+.btn-right {
+  position: absolute;
+  right: 0;
+  top: 0;
+  background-color: var(--primary-color);
+  border: none;
+  margin-top: 10px;
+  margin-right: 10px;
+}
+
+.btn-left {
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: var(--primary-color);
+  border: none;
+  margin-top: 0px;
+  margin-right: 10px;
+}
+
+#btn-left {
+  width: 30px;
+  margin-top: 4px;
+}
+
+.div-menu {
+  padding-top: 32px;
+  padding-right: 20px;
+  display: none;
+}
+
+.div-menu ul {
+  list-style: none;
+  text-align: left;
+  margin: 0;
+  padding: 0;
+  border: 1px solid var(--secondary-color);
+  border-radius: 4px;
+  background-color: white;
+  box-shadow: -6px 4px 10px var(--secondary-color);
+}
+
+.div-menu li {
+  line-height: 1.5;
+  position: relative;
+}
+
+.div-menu button {
+  color: var(--txt-color);
+  border: 0;
+  background-color: white;
+  font-size: 16px;
+}
+
+.div-menu button:hover,
+.div-menu li:hover,
+.div-menu li:hover > button {
+  background-color: var(--secondary-color);
+}
+</style>
