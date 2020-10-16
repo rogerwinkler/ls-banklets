a4f02ac10a2ed0ca43ade3052f032d6c0ae6bd8a
ojehle <ojehle@yahoo.com>
2020-10-16 09:38:11 +0200

    baseURL missing comma

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   env: {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000'
-  }
+  },
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
