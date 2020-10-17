47ab24c86d9b3e4aef3440b090f0462667365d69
rw <roger.winkler@monex.li>
2020-10-17 12:45:23 +0200

    favicon set to mx logo, rounded corner shape

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,13 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/img/logo-mx-cubic-32x32-trans.png"
+      }
+    ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
