bc5172709b1bc7bd5bae6e4861293123668a9b2f
ojehle <ojehle@yahoo.com>
2020-11-23 12:53:56 +0100

    add baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
+  env: {
+    baseUrl: process.env.BASE_URL || "http://localhost:3000"
+  },
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
